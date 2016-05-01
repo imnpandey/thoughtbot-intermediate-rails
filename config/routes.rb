6f4d335c28623b8e3ddcbde9395346df07335e24
Navneet Pandey <npandey057@gmail.com>
2016-05-01 13:54:17 +0530

    add dashboard barebones

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  # only add in the routes you are actually using
   root to:'homes#show', via: :get
+  resource :dashboard, only: [:show] #singleton resource
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
 end
