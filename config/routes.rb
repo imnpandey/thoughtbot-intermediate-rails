ac6207e3f3d208681e20278ca62a54f8e8a2c93b
Navneet Pandey <npandey057@gmail.com>
2016-05-01 14:15:42 +0530

    add shouts functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resource :dashboard, only: [:show] #singleton resource
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resources :shouts, only: [:create]
 end
+
