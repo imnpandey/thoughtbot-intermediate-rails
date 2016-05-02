dda001c68e3529859625e34321d086d148e8b63e
Navneet Pandey <npandey057@gmail.com>
2016-05-02 11:36:35 +0530

    add following logic

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to:'homes#show', via: :get
   resource :dashboard, only: [:show] #singleton resource
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show] do
+    post 'follow' => 'following_relationships#create'
+  end
   resources :shouts, only: [:show]
   resources :text_shouts, only: [:create]
   resources :photo_shouts, only: [:create]
