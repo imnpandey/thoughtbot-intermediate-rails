b725322a67240b5beaa128e6b1264e872153c37f
Navneet Pandey <npandey057@gmail.com>
2016-05-02 17:04:30 +0530

    add follow associations in concrens

--- app/models/concerns/followable.rb
+++ app/models/concerns/followable.rb
@@ -1,6 +1,19 @@
 module Followable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :followed_user_relationships,
+      foreign_key: :follower_id,
+      class_name: 'FollowingRelationship'
+    has_many :followed_users, through: :followed_user_relationships
+
+    has_many :follower_relationships,
+      foreign_key: :followed_user_id,
+      class_name: 'FollowingRelationship'
+    has_many :followers, through: :follower_relationships
+  end
+
   def following? user
-    # followed_user_ids from has many relation
     followed_user_ids.include? user.id
   end
 
